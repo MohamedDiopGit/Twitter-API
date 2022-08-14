60fe5fe70f53579bf537dcf754b04866b8d90fcd
MohamedDiopGit <diopm.contact@gmail.com>
2022-08-14 21:56:25 +0200

    Final commit for the get_user_followers file

--- twitter-api.py
+++ twitter-api.py
@@ -1,27 +1,14 @@
 # import the module
 import tweepy
-
-# assign the values accordingly
-
+import config
 
 # authorization of consumer key and consumer secret
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-# set access to user's access key and access secret
-auth.set_access_token(access_token, access_token_secret)
-
-# calling the api
-api = tweepy.API(auth)
-# the screen_name of the targeted user
-user = api.get_users_following("")
-
-
+client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
  
-# printing the latest 20 followers of the user
-# for follower in api.followers(screen_name):
-# 	print(follower.screen_name)
+users = client.get_users_followers(id=config.USER_ID)
 
-print('Latest Followers:')
-for follower in user.followers():
-    print('Name: ' + str(follower.name))
-    print('Username: ' + str(follower.screen_name))
+for user in users.data:
+    print(user.keys)
+
+print("Number of followers displayed: ",len(users.data))
